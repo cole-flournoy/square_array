f2f08a7bf80b35bdee28d1807203e5b6427903ea
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-09 17:08:22 -0500

    commit tree 9277789076c59d0e4352b35ca0e2f938814136ee

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |element|
+    element = element**2
+    new_array = new_array << element
+  end
+  new_array
 end
+
+original = [2, 4, 6, 8]
+square_array(original)
+
